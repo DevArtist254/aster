3fb4505db5f03d61a4740302c186b89b39f4abcf
DevArtist254 <kevinwasonga415@outlook.com>
2025-11-07 06:09:51 +0300

    utilRP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1 class="--ff-accent --clr-light-400 --fs-large-200">Hello world</h1>
+  <nav class="flex-row space-between padding-400 | --clr-dark-400 --fs-small-600">
+    <h3>Aster</h3>
+    <ul class="flex-row | --fs-small-200 --ff-base">
+      <li class="padding-200">About us</li>
+      <li class="padding-200">Property</li>
+      <li class="padding-200">Agent</li>
+      <li class="padding-200 round-xl | --bdg-dark-400 --clr-light-000">Contact us</li>
+    </ul>
+  </nav>
+  <main>
+  </main>
 </template>
 
 <script>
